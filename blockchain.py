24c3c989e9a14ad6ded22b3568a5bef505bfa377
shresthabijay <sbijay777@gmail.com>
2019-01-08 11:46:01 +0545

    blockchain validity check

--- blockchain.py
+++ blockchain.py
@@ -1,5 +1,8 @@
-from flask import Flask
+from flask import Flask,jsonify
+import json
 import datetime
+import hashlib
+
 app = Flask(__name__)
 
 class Blockchain:
@@ -14,10 +17,41 @@ class Blockchain:
             'previous_hash':previous_hash
          }
       self.chain.append(block)
+   
+   def get_previous_block(self):
+      return self.chain[-1]
 
-@app.route('/')
-def hello_name():
-   return "karun boka ho!"
+   def proof_of_work(self,previous_proof):
+      new_proof=1
+      check_proof=False
+      while check_proof is False:
+         hash_operation=hashlib.sha256(str(new_proof**2-previous_proof**2).encode()).hexdigest()
+         if hash_operation[:4] == "0000":
+            check_proof=True
+         else:
+            new_proof+=1
+      return new_proof
 
-if __name__ == '__main__':
-   app.run()
+   def hash(self,block):
+      encoded_block=json.dumps(block,sort_keys=True).encode()
+      return hashlib.sha256(encoded_block).hexdigest()
+
+   def is_chain_valid(self,chain):
+      previous_block=chain[0]
+      block_index=1
+      while block_index<len(chain):
+         block=chain[block_index]
+         if block["previous_hash"] != self.hash(previous_block):
+            return False
+         previous_proof=previous_block["proof"]
+         proof=block["proof"]
+         hash_operation=hashlib.sha256(str( proof**2-previous_proof**2).encode()).hexdigest()
+         if hash_operation[:4] != "0000":
+            return False
+         previous_block=block
+         block_index+=1
+      
+      return True
+      
+
+
